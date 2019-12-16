0de5cf8e94a2a2cf095100dae164ad7a5dc4a033
Nicklas Nielsen <nicklas.bring.nielsen@gmail.com>
2019-12-16 19:43:51 +0100

    Created Api router

--- routes/api.js
+++ routes/api.js
@@ -5,11 +5,7 @@ var db = require('../db/dbLogic');
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
-  res.render('index', {
-    title: 'Skibsdata', 
-    ships: await db.getShips(),
-    sensorer: await db.getSensorData()
-    });
+  res.send("hej");
 });
 
 module.exports = router;
