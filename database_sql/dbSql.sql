d64b513dcfba186c14cdfc6395ed9dae9373ae84
Nicklas Nielsen <nicklas.bring.nielsen@gmail.com>
2019-12-17 16:44:34 +0100

    ..

--- database_sql/dbSql.sql
+++ database_sql/dbSql.sql
@@ -22,7 +22,7 @@
 DROP SCHEMA IF EXISTS TwinShips;
 CREATE DATABASE TwinShips;
 USE TwinShips;
-
+ 
 DROP TABLE IF EXISTS `sensordata`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
